3f4fa274ef1bbdcaca3e710c01620368dd20a5de
Lucas Ramos <ramosilucas@gmail.com>
2021-01-24 15:26:02 -0300

    Commit para pruebas v0.2

--- fxs.js
+++ fxs.js
@@ -9,14 +9,16 @@ $(function(){
 	FechaEvento.minute(15)
 
 	// Hora Argentina
-	HoraArg = moment.tz(FechaEvento, 'America/Buenos_Aires');
+	HoraArg = moment.tz(FechaEvento.format("YYYY-MM-DD HH:mm:ss"), 'America/Buenos_Aires');
 
 	// Hora Usuario
-	HoraUsuario = moment.tz(FechaEvento, ZonaUsuario);
+	HoraUsuario = moment.tz(FechaEvento.format("YYYY-MM-DD HH:mm:ss"), ZonaUsuario);
 
 	htmlArg = HoraArg.format("DD/MM/YYYY HH:mm (ZZ)") + " | Zona horaria: America/Buenos_Aires"
 	$("#HoraArg").html(htmlArg)
 
 	htmlUsuario = HoraUsuario.format("DD/MM/YYYY HH:mm (ZZ)") + " | Zona horaria (detectada): " + ZonaUsuario
 	$("#HoraUsuario").html(htmlUsuario)	
+
+	console.log("v0.2")
 })
