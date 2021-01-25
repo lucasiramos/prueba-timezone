4a74d03448991bba341499e3ecf1fb2040d1fe11
Lucas Ramos <ramosilucas@gmail.com>
2021-01-25 09:41:21 -0300

    Version 0.5

--- fxs.js
+++ fxs.js
@@ -1,18 +1,19 @@
 $(function(){
 	ZonaUsuario = moment.tz.guess()
-	FechaEvento = moment()
+	/*FechaEvento = moment()
 
 	FechaEvento.date(9)
 	FechaEvento.month(6)
 	FechaEvento.year(2021)
 	FechaEvento.hour(12)
-	FechaEvento.minute(15)
+	FechaEvento.minute(15)*/
 
 	// Hora Argentina
-	HoraArg = moment.tz(FechaEvento.format(), 'America/Buenos_Aires');
+	//FechaEvento.format()
+	HoraArg = moment.tz('2021-07-09 12:15:00', 'America/Buenos_Aires');
 
 	// Hora Usuario
-	HoraUsuario = moment.tz(FechaEvento.format(), ZonaUsuario);
+	HoraUsuario = moment.tz('2021-07-09 12:15:00', ZonaUsuario);
 
 	htmlArg = HoraArg.format("DD/MM/YYYY HH:mm (ZZ)") + " | Zona horaria: America/Buenos_Aires"
 	$("#HoraArg").html(htmlArg)
@@ -20,5 +21,5 @@ $(function(){
 	htmlUsuario = HoraUsuario.format("DD/MM/YYYY HH:mm (ZZ)") + " | Zona horaria (detectada): " + ZonaUsuario
 	$("#HoraUsuario").html(htmlUsuario)	
 
-	console.log("v0.4")
+	console.log("v0.5")
 })
